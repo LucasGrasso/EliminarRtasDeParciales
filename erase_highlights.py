a542573f1ebb87eccca1e52002784cb227462b50
Lucas Grasso <lucasgrassoramos@gmail.com>
2023-02-26 16:50:43 -0300

    unnecesary checks

--- erase_highlights.py
+++ erase_highlights.py
@@ -15,11 +15,7 @@ def erase_highlights(image: Image.Image) -> Image.Image:
     for x in range(image.width):
         for y in range(image.height):
             pixel_color = image.getpixel((x, y))
-            if (120, 65, 1) <= pixel_color <= (255, 255, 150) or (
-                200,
-                0,
-                0,
-            ) <= pixel_color <= (255, 0, 0):
+            if (120, 65, 1) <= pixel_color <= (255, 255, 150):
                 image.putpixel((x, y), (255, 255, 255))  # Set pixel to white
 
     return image
